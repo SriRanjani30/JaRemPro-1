16e93439f17d980d3f4a7522bfade9bfba1a5c1f
SriRanjani30 <Sriranjani935@gmail.com>
2025-09-17 13:43:09 +0000

    JaRemPro Pototype 1

--- JaRemPro.java
+++ JaRemPro.java
@@ -1,7 +1,17 @@
 import java.awt.Image;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.URL;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.Timer;
@@ -11,21 +21,36 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 
-public class JaRemPro {
+public class JaRemPro implements Serializable{
     private static final ScheduledExecutorService scheduler= Executors.newSingleThreadScheduledExecutor();
-    private static final List<String> reminders=new ArrayList<>();
-    private static final String [] iconPaths={"icons/Bird.png","icons/Clock.png","icons/Dog.png","icons/Hen.png","icons/Snail.png","icons/Monitor.png","icons/Plane.png"};
+    public static final List<String> reminders=new ArrayList<>();
+    private static final URL [] iconPaths={JaRemPro.class.getResource("/Images/icons/Bird.png"),JaRemPro.class.getResource("/Images/icons/Clock.png"),JaRemPro.class.getResource("/Images/icons/Dog.png"),JaRemPro.class.getResource("/Images/icons/Hen.png"),JaRemPro.class.getResource("/Images/icons/Snail.png"),JaRemPro.class.getResource("/Images/icons/Monitor.png"),JaRemPro.class.getResource("/Images/icons/Plane.png")};
     private static final Random rand = new Random();
+    public static List<String> tasks = new ArrayList<>();
+    public static List<String> times = new ArrayList<>();
+    public static List<Date> dates = new ArrayList<>();
+    public static final String FILE_NAME = "reminders.dat";
+
+    public JaRemPro(List<String> tasks, List<String> times, List<Date> remDates){
+        this.tasks=tasks;
+        this.times=times;
+        this.dates=remDates;
+    }
     public static void main(String[] args) {
-         if(args.length>0){
+        JaRemPro saved = JaRemPro.loadReminders();
+        if(saved!=null){
+            showReminder(saved.tasks, saved.times, saved.dates);
+        }
+         Date today = new Date();
+         if(args.length>=2){
         
+        }
+        else if(args.length == 1 && args[0].equalsIgnoreCase("-silent")){
+
         }
         else{
             SwingUtilities.invokeLater(()->new reminderInputGUI());
         }
-        List<String> tasks = new ArrayList<>();
-        List<String> times = new ArrayList<>();
-        String[] options={"Ok","Snooze"};
         for (int i = 0; i < args.length; i++) {
             if (args[i].equalsIgnoreCase("-task")) {
                 StringBuilder taskBuilder=new StringBuilder();
@@ -66,6 +91,7 @@ public class JaRemPro {
                 }
                 else{
                     times.add(timeStr);
+                    dates.add(today);
                 }
                 i=k-1;
             }
@@ -75,11 +101,21 @@ public class JaRemPro {
             frame.setAlwaysOnTop(true);
             JOptionPane.showMessageDialog(frame, "Tasks and times do not match. Please check your input.", "JaRemPro", JOptionPane.WARNING_MESSAGE);
             frame.dispose();
+            return;
         }
-        else{
+        showReminder(tasks, times, dates);
+    }
+    private static ImageIcon randomIcon(int height, int width){
+            URL path=iconPaths[rand.nextInt(iconPaths.length)];
+        return new ImageIcon(new ImageIcon(path).getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+
+    }
+    public static void showReminder(List<String> tasks, List<String> times, List<Date> remDate) {
+        String[] options={"Ok","Snooze"};
         for (int i = 0; i < tasks.size(); i++) {
             String time=times.get(i);
             String task=tasks.get(i);
+            Date date=remDate.get(i);
             time = time.trim().toUpperCase();
             LocalTime remTime;
             if (time.contains("AM") || time.contains("PM")) {
@@ -90,8 +126,18 @@ public class JaRemPro {
                 DateTimeFormatter formatter24 = DateTimeFormatter.ofPattern("HH:mm");
                 remTime = LocalTime.parse(time, formatter24);
             }
-            LocalTime currentTime = LocalTime.now();
-            long delay = java.time.Duration.between(currentTime, remTime).toMillis();
+            int hour = remTime.getHour();
+            int minute =  remTime.getMinute();
+
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            cal.set(Calendar.HOUR_OF_DAY, hour);
+            cal.set(Calendar.MINUTE, minute);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            Date finalRemTime = cal.getTime(); 
+
+            long delay = finalRemTime.getTime() - System.currentTimeMillis();
             final String remTask = task;
             if (delay <= 0) {
                 JFrame frame=new JFrame();
@@ -102,22 +148,30 @@ public class JaRemPro {
             } else {
                 Timer timer = new Timer();
                 timer.schedule(new TimerTask() {
+                    @Override
                     public void run() {
                         JFrame frame=new JFrame();
                         frame.setAlwaysOnTop(true);
                         ImageIcon icon=randomIcon(50, 50);
                         SwingUtilities.invokeLater(()->{
                             int choice=JOptionPane.showOptionDialog(frame, remTask, "JaRemPro - Reminder", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, icon, options, options[0]);
+                            int index = tasks.indexOf(remTask);
                             switch (choice){
                                 case 0:
-                                reminders.remove(remTask);
+                                if(!tasks.isEmpty() && !times.isEmpty() && !remDate.isEmpty() && index>0 && index<=tasks.size()){
+                                reminders.remove(index);
+                                tasks.remove(index);
+                                times.remove(index);
+                                remDate.remove(index);
+                                JaRemPro remainingReminders1 = new JaRemPro(tasks, times, remDate);
+                                saveReminders(remainingReminders1);
+                                }
                                 if(reminders.isEmpty()){
                                     System.exit(0);
                                 }
                                 frame.dispose();
                                 break;
-                                
-                                case 1:
+                                case 1:{
                                 String input=JOptionPane.showInputDialog(frame,"Snooze time in minutes:","JaRemPro",JOptionPane.QUESTION_MESSAGE);
                                 try{
                                 int snoozeTime=Integer.parseInt(input);
@@ -126,30 +180,50 @@ public class JaRemPro {
                                 catch(NumberFormatException e){
                                     JOptionPane.showMessageDialog(frame, "Invalid Snooze Time", "JaRemPro", JOptionPane.WARNING_MESSAGE);
                                 }
-                                break;
+
+                            }break;
 
                             case -1:
+                                if(!tasks.isEmpty() && !times.isEmpty() && !remDate.isEmpty() && index>0 && index<=tasks.size()){
+                                reminders.remove(index);
+                                tasks.remove(index);
+                                times.remove(index);
+                                remDate.remove(index);
+                                JaRemPro remainingReminders2 = new JaRemPro(tasks, times, remDate);
+                                saveReminders(remainingReminders2);
+                                }
                                 System.exit(0);
                                 break;
-
-                            default:
-                                System.out.println("Unknown Choice");
-                                break;
                             
-                            }
+                        }
                           });
                         java.awt.Toolkit.getDefaultToolkit().beep();
                         timer.cancel();
                     }
                 }, delay);
             }
-            System.out.println("Reminder set for " + remTime + " : " + task);
+            JOptionPane.showMessageDialog(null, "Reminder "+task+" : "+time+" added successfully!","JaRemPro",JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    }
-    private static ImageIcon randomIcon(int height, int width){
-        String path=iconPaths[rand.nextInt(iconPaths.length)];
-        return new ImageIcon(new ImageIcon(path).getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+    public static void saveReminders(JaRemPro remainingReminders){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))){
+            oos.writeObject(remainingReminders);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
 
+    }
+
+    public static JaRemPro loadReminders(){
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))){
+            return (JaRemPro) ois.readObject();
+        }
+        catch(FileNotFoundException e){
+            return null;
+        }
+        catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- reminderInputGUI.java
+++ reminderInputGUI.java
@@ -1,5 +1,5 @@
-import com.sun.tools.javac.Main;
 import java.awt.*;
+import java.util.Date;
 import javax.swing.*;
 
 public class reminderInputGUI extends JFrame{
@@ -7,67 +7,106 @@ public class reminderInputGUI extends JFrame{
     private JTextField timeField;
 
     public reminderInputGUI(){
-        setLayout(new GridBagLayout());
+        JPanel content = new JPanel(new GridBagLayout());
+        content.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+        setContentPane(content);
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5,5,5,5);
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        ImageIcon bannerImage = new ImageIcon("Banner.jpg");
-        Image scaledImage = bannerImage.getImage().getScaledInstance(300, 80, Image.SCALE_SMOOTH);
+        ImageIcon bannerImage = new ImageIcon(reminderInputGUI.class.getResource("/Images/Banner.png"));
+        Image scaledImage = bannerImage.getImage().getScaledInstance(350, 125, Image.SCALE_SMOOTH);
         bannerImage=new ImageIcon(scaledImage);
         JLabel Banner = new JLabel(bannerImage);
 
-        gbc.gridx = 0;
+        gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.gridwidth = 2;
+        gbc.gridwidth = 4;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 5, 10, 5);
-        add(Banner, gbc);
+        content.add(Banner, gbc);
 
-        gbc.insets = new Insets(5,5,5,5);
-        gbc.gridx = 0;
+        gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.NONE;
+        content.add(new JLabel ("Task"), gbc);
+
+        taskField = new JTextField(24);
+        Dimension d1 = taskField.getPreferredSize();
+        d1.height = 24;
+        taskField.setPreferredSize(d1);
+        gbc.weightx = 1.0;
+        gbc.gridwidth = 4;
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        content.add(taskField,gbc);
+
         gbc.gridwidth = 1;
-        add(new JLabel ("Task"), gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        add(new JTextField(15),gbc);
-
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        add(new JLabel("Time"),gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 2;
-        add(new JTextField(15),gbc);
+        content.add(new JLabel("Time"),gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.gridwidth=2;
-        add(new JButton("Add Reminder"), gbc);
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.NONE;
+        timeField = new JTextField(10);
+        Dimension d2 = timeField.getPreferredSize();
+        d2.height = 24;
+        timeField.setPreferredSize(d2);
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        content.add(timeField,gbc);
 
-        ImageIcon icon = new ImageIcon("JaRemPro.png");
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        content.add(new JLabel("Date"), gbc);
+
+        JSpinner dateSpinner = new JSpinner(new SpinnerDateModel());
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(dateSpinner,"dd-MM-yyyy");
+        dateSpinner.setEditor(dateEditor);
+        dateSpinner.setPreferredSize(new Dimension(140, 25));
+        gbc.gridx = 4;
+        gbc.gridy = 2;
+        content.add(dateSpinner, gbc);
+
+        JButton button = new JButton("Add Reminder");
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        button.setPreferredSize(new Dimension(24, 30));
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        gbc.gridwidth=5;
+        content.add(button, gbc);
+
+
+        ImageIcon icon = new ImageIcon(reminderInputGUI.class.getResource("/Images/JaRemPro.png"));
         setIconImage(icon.getImage());
 
 
         setTitle("JaRemPro");
         pack();
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
         setResizable(false);
+
+        button.addActionListener(e -> {
+        String task=taskField.getText().trim();
+        String time=timeField.getText().trim();
+        Date remDate = (Date) dateSpinner.getValue();
+        addReminder(task,time, remDate);
+        });
 
 
 
     }
-    private void addReminder(){
-        String task=taskField.getText().trim();
-        String time=timeField.getText().trim();
-
+    private void addReminder(String task, String time, Date remDate){
         if(!task.isEmpty() && !time.isEmpty()){
-            JOptionPane.showMessageDialog(this, "Reminder "+task+" : "+time+" added successfully!","JaRemPro",JOptionPane.INFORMATION_MESSAGE);
+            JaRemPro.tasks.add(task);
+            JaRemPro.times.add(time);
+            JaRemPro.dates.add(remDate);       
+            JaRemPro.reminders.add(task);
+            JaRemPro.showReminder(JaRemPro.tasks, JaRemPro.times, JaRemPro.dates);
         }
         else{
             JOptionPane.showMessageDialog(this, "Please enter both task and time.","JaRemPro",JOptionPane.WARNING_MESSAGE);
